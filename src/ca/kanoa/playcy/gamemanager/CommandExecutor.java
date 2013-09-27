0e8928d57b2d2ee0a09496da149257e25e2e327a
EvilKanoa <kanoa@kanoa.ca>
2013-09-27 01:12:09 -0300

    Added version command

--- src/ca/kanoa/playcy/gamemanager/CommandExecutor.java
+++ src/ca/kanoa/playcy/gamemanager/CommandExecutor.java
@@ -1,5 +1,6 @@
 package ca.kanoa.playcy.gamemanager;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 
@@ -8,8 +9,17 @@ public class CommandExecutor implements org.bukkit.command.CommandExecutor {
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd,
 			String label, String[] args) {
-		// TODO Auto-generated method stub
-		return false;
+		if (args.length > 0 && args[0].equalsIgnoreCase("addloc")) {
+			
+			return true;
+		} else if (args.length > 0 && args[0].equalsIgnoreCase("version")) {
+			sender.sendMessage(ChatColor.GREEN + "[" + 
+					PlayCY.getInstance().getDescription().getName() + 
+					"] version: " + PlayCY.getInstance().getDescription().getVersion());
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 }
